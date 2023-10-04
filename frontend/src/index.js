47b1543c0d97ccdea08ea7e7581ad230b293fb43
AMS <111883236+AMS003010@users.noreply.github.com>
2023-10-04 20:37:13 +0530

    favicon on tab switch

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const favicon = document.querySelector('link[rel="icon"]')
+
+document.addEventListener("visibilitychange", () => {
+    const hidden = document.hidden
+
+    favicon.setAttribute(
+        "href", 
+        `/favicon${hidden ? "-hidden" : ""}.ico`
+    )
+})
+
 root.render(
   <React.StrictMode>
     <App />
