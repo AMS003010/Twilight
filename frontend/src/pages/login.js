13db6f3435fb8f299717733fc929dab468dabe99
AMS <111883236+AMS003010@users.noreply.github.com>
2023-10-07 00:21:30 +0530

    login page layout

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -13,8 +13,28 @@ class Login extends React.Component {
                 <nav>
                     <Logo/>
                 </nav>
+                <div className='logindiv'>
+                    <div className='loginForm'>
+                        <form>
+                            <h1 className='loginHeading'>Login</h1>
+                            <div className='loginCenter'>
+                                <div className='loginEmail'>
+                                    <label>Email*</label><br/>
+                                    <input type='email'/>
+                                </div>
+                                <div className='loginPassword'>
+                                    <label>Password*</label><br/>
+                                    <input type='password'/>
+                                </div>
+                            </div>
+                            <div className='loginButton'>
+                                <button>Login</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className='loginCoverImage'></div>
+                </div>
                 <div className='loginUnderLay'></div>
-                <div></div>
             </div>
         )
     }
