8e3966cf6eb890bf82738d3ef7e9c9a20e2bee5b
AMS <111883236+AMS003010@users.noreply.github.com>
2023-10-11 22:51:43 +0530

    Working on user context

---
+++ frontend/src/context/UserContext.js
@@ -0,0 +1,32 @@
+import {createContext,useEffect,useReducer} from 'react'
+
+export const UserContext = createContext()
+
+export const userReducer = (state,action) => {
+    switch(action.type){
+        case 'LOGIN':
+            return {user: action.payload}
+        case 'LOGOUT':
+            return {user: null}
+        default:
+            return state
+    }
+}
+
+export const UserContextProvider = ({children}) => {
+    const [state,dispatch] = useReducer(userReducer,{
+        user: null
+    })
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'))
+        if(user){
+            dispatch({type : 'LOGIN' ,payload :user})
+        }
+    },[])
+    console.log('UserContext state: ',state)
+    return(
+        <UserContext.Provider>
+            {children}
+        </UserContext.Provider>
+    )
+}
