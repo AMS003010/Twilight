88af7c930f1e82bcc40c6c9f5e848250ea449162
AMS <111883236+AMS003010@users.noreply.github.com>
2023-10-26 23:58:13 +0530

    Updated musicPlayer.js

--- frontend/src/components/musicPlayer.js
+++ frontend/src/components/musicPlayer.js
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
 
+import playIcon from '../img/play.png'
+import pauseIcon from '../img/pause.png'
+import soundIcon from '../img/sound.png'
+
 const MusicPlayer = ({ song, setSong }) => {
   const [audio] = useState(new Audio());
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(true);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
   const [volume, setVolume] = useState(1);
 
+  // Add a useEffect that listens for changes in the 'song' state
   useEffect(() => {
     if (song) {
       audio.src = song.song;
@@ -27,6 +32,11 @@ const MusicPlayer = ({ song, setSong }) => {
       audio.removeEventListener("loadeddata", handleLoadedData);
     };
   }, [song, isPlaying, volume]);
+
+  // Use this useEffect to set 'isPlaying' to true when 'song' changes
+  useEffect(() => {
+    setIsPlaying(true);
+  }, [song]);
 
   const togglePlayPause = () => {
     setIsPlaying(!isPlaying);
@@ -54,18 +64,25 @@ const MusicPlayer = ({ song, setSong }) => {
     <div>
       {song ? (
         <div className="musicPlayerContainer" style={{ display: "flex" }}>
-          <audio controls id="audio-element" hidden></audio>
-          <button onClick={togglePlayPause}>
-            {isPlaying ? "Pause" : "Play"}
+          <audio controls id="audio-element" hidden autoPlay></audio>
+          <div style={{backgroundImage:`url('${song.img}')`}} className="muPlayPic"></div>
+          <div className="muPlName">
+            <div style={{fontWeight:'600'}}>{song.name}</div>
+            <div style={{fontWeight:'500'}}>{song.artist}</div>
+          </div>
+          <button onClick={togglePlayPause} style={{backgroundImage:`url('${isPlaying ? pauseIcon : playIcon}')`}} className="muPlayBut">
           </button>
+          <div style={{width:'30px',marginRight:'25px'}}>{formatTime(currentTime)}</div>
           <input
             type="range"
             min={0}
             max={duration}
             value={currentTime}
             onChange={handleSliderChange}
+            style={{marginRight:'20px',width:'300px'}}
           />
-          <span>{formatTime(currentTime)}</span> / {formatTime(duration)}
+          <div>{formatTime(duration)}</div>
+          <img src={soundIcon} alt="k" style={{width:'25px',marginLeft:'220px',marginRight:'10px'}}  />
           <input
             type="range"
             min={0}
@@ -90,3 +107,5 @@ function formatTime(seconds) {
   const remainingSeconds = Math.floor(seconds % 60);
   return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
 }
+
+// Left part of the currentTime span is moving as the numbers change
